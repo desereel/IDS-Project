a0a0f1738ac03d697e84ed250ce4f06386cd36e9
Deseree Frink-Larcart <dfrinkla@nd.edu>
2023-04-17 13:11:14 -0400

    imports building

---
+++ java/src/main/java/edu/nd/cse/ids/stocks/StockNLG.java
@@ -0,0 +1,19 @@
+package edu.nd.cse.ids.stocks;
+
+import edu.nd.cse.ids.stocks.messages.*;
+
+import java.util.List;
+import java.util.LinkedList;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.Option;
+
+import simplenlg.framework.*;
+import simplenlg.lexicon.*;
+import simplenlg.realiser.english.*;
+import simplenlg.phrasespec.*;
+import simplenlg.features.*;
