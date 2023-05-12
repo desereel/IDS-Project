8be0da550acc62e7859cddeca97d7b7775560a8f
Deseree Frink-Larcart <dfrinkla@nd.edu>
2023-05-12 10:24:48 -0400

    messages fix

--- python/messages/CurrPriceMessage.py
+++ python/messages/CurrPriceMessage.py
@@ -5,11 +5,12 @@ import sys
 ticker = sys.argv[1]
 
 # Get the stock data for the ticker
-stock_data = yf.Ticker("ticker")
+stock_data = yf.Ticker(ticker)
 
 # Retrieve the current share price
 currPrice = stock_data.info['currentPrice']
 
 # Print the volume data as a string
 print(str(currPrice))
+
 
